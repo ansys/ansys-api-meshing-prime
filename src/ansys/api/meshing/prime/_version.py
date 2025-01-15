7fba7c76128a2fa3938fa16653cc873b5a918110
hbroichh <heike.broichhausen@ansys.com>
2025-01-15 10:57:56 +0000

    can we do this?

---
+++ src/ansys/api/meshing/prime/_version.py
@@ -0,0 +1,26 @@
+"""Version of ansys-api-meshing-prime library.
+
+On the ``main`` branch, use 'dev0' to denote a development version.
+For example:
+
+version_info = 0, 1, 0, 'dev0'
+
+Examples
+--------
+Print the version
+
+>>> from ansys.product import library
+>>> print(library.__version__)
+0.1.4
+
+"""
+
+# major, minor, patch
+version_info = 0, 1, 4
+
+# Nice string for the version
+__version__ = '.'.join(map(str, version_info))
+
+# Write the version string to a file
+with open('VERSION', 'w') as version_file:
+    version_file.write(__version__)
