310c4d6e651ffffb729069564e427047bd0545cd
Subha Nayak <118820636+subhnayak@users.noreply.github.com>
2026-01-13 15:55:08 +0530

    update to fix 0.1.5 version

--- src/ansys/api/meshing/prime/_version.py
+++ src/ansys/api/meshing/prime/_version.py
@@ -16,7 +16,7 @@ Print the version
 """
 
 # major, minor, patch
-version_info = 0, 1, 5, 'dev1'
+version_info = 0, 1, 5
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
